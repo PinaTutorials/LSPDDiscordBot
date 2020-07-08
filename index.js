8aea83864f2676d08e383afeb6de9a82d0effddf
PinaTutorials <41023844+PinaTutorials@users.noreply.github.com>
2020-07-08 11:16:15 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+
+const bot = new Discord.Client();
+
+bot.on('ready', () => {
+
+    console.log('I am ready!');
+	message.send('Hello everybody!!');
+
+});
+
+bot.on('message', message => {
+
+    if (message.content === 'ping') {
+
+       message.reply('pong');
+
+       }
+
+});
+
+bot.login(process.env.BOT_TOKEN);
