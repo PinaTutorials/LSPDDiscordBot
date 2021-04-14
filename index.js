868a86ee34d370784bddf3caa9861a3293ccaa68
PinaTutorials <41023844+PinaTutorials@users.noreply.github.com>
2021-04-14 17:03:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,49 +1,48 @@
 const Discord = require('discord.js');
 let db = require("mysql-sync-query");
 let databaseObject = new db("sql11405372");
-
-var niggerCount = 0;
-var finalCount = 0;
-
-databaseObject.connectLocal("sql11.freemysqlhosting.net", 3306, "sql11405372", "Kjct3ReGFb"); 
+var fs = require("fs");
 
 const bot = new Discord.Client();
 
-bot.on('message', message => {
+var finalCount = 0;
+var fileString = fs.readFileSync("./badWords.txt", "utf-8").toLowerCase();
+var fileWords = fileString.split("\n");
+
+databaseObject.connectLocal("sql11.freemysqlhosting.net", 3306, "sql11405372", "Kjct3ReGFb");
+
+bot.on('message', async message => {
+	
+	var messageString = message + '';
 	
 	if(message.author.id == 538665836911788039){
 		return;
 	}
 	
 	if(message.content.toLowerCase() === "!badwords"){
-		var res = await databaseObject.executeQuery("SELECT COUNT(*) AS count FROM User WHERE user = " + message.author.id);
-		if(res[0].count == 0){
-			var res1 = await databaseObject.executeQuery("SELECT occurance FROM User WHERE user = " + message.author.id);
-			message.channel.send("<@" + message.author.id + "> You said a total of " + res[0].occurance + " bad words.");
+		await badWords(message); 
+	}
+	else if(message.content.toLowerCase().includes("!addbadword")){
+		var badWordToAdd = messageString.slice(messageString.indexOf(' ') + 1, messageString.length).trim();
+		if(! await checkForBadWord(badWordToAdd)){
+			fileString += "\n" + badWordToAdd.toLowerCase();
+			await writeToFile(fileString);
+			message.channel.send("Word " + badWordToAdd + " will now be accounted for.");
 		}
 		else{
-			message.channel.send("<@" + message.author.id + "> You haven't said a bad word!");
+			message.channel.send("Word " + badWordToAdd + " is already being accounted for.");
 		}
 	}
+	else if(message.content.toLowerCase() === "!helpwords"){
+		message.channel.send("List of commands: \n-!addbadword: Adds a word to be counted, OBS: I can't remove it, talk to Creator to remove a word");
+	}
 	else{
-		var string = message + '';
-
-		var words = string.split(" ");
-
-		var count = words.length;
-
-		for(var i = 0; i < count; i++){
-			switch(words[i].toLowerCase()){
-				case 'nigga': finalCount++; break;
-				case 'preto': finalCount++; break;
-				case 'negro': finalCount++; break;
-				case 'nigger': finalCount++; break;
-				case 'niggers': finalCount++; break;
-				case 'n4gger': finalCount++; break;
-				case 'n4gger': finalCount++; break;
-				case 'bai': if(words[i + 1] == 'bunda') finalCount++; break;
-				case 'nice': if(words[i + 1] == 'cock') finalCount++; break;
-				case 'tem': if(words[i + 1] == 'glema') finalCount++; break;
+		for(var i = 0; i < fileWords.length; i++){
+			if(messageString.includes(fileWords[i].replace(/(\r\n|\n|\r)/gm, ""))){
+				if(fileWords[i] != ''){
+					console.log("yes");
+					finalCount++;
+				}
 			}
 		}
 		if(finalCount != 0){		
@@ -51,21 +50,48 @@ bot.on('message', message => {
 			message.react("🙊");
 		}
 		finalCount = 0;
+		
 	}
 });
 
-bot.login(process.env.BOT_KEY);
+bot.login("NTM4NjY1ODM2OTExNzg4MDM5.XEw1pw.LnDQcgfQsTDFuTqyeBYgnPUNV_Q");
+
+async function checkForBadWord(badWord){
+	for(var i = 0; i < fileWords.length; i++){
+		if(badWord.includes(fileWords[i].replace(/(\r\n|\n|\r)/gm, ""))){
+			if(fileWords[i] != ''){
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+async function writeToFile(fileString){
+	fs.writeFileSync('./badWords.txt', fileString);
+	fileWords = fileString.split("\n");
+}
+
+async function badWords(message){
+	var res = await databaseObject.executeQuery("SELECT COUNT(*) AS count FROM User WHERE user = " + message.author.id);
+	if(res[0].count == 0){
+		message.channel.send("<@" + message.author.id + "> You haven't said a bad word!");
+	}
+	else{
+		res = await databaseObject.executeQuery("SELECT occurance FROM User WHERE user = " + message.author.id);
+		message.channel.send("<@" + message.author.id + "> You said a total of " + res[0].occurance + " bad words.");
+	}
+}
 
 async function badWordFound(message, count){
 	var res = await databaseObject.executeQuery("SELECT COUNT(*) AS count FROM User WHERE user = " + message.author.id);
 	if(res[0].count == 0){
 		var res = await databaseObject.executeQuery("INSERT INTO User (user, occurance) VALUES (" + message.author.id + ", " + count + ")");
-		niggerCount = count;
 		if(parseInt(count) == 1){
 			message.channel.send("<@" + message.author.id + "> You said a bad word!.");
 		}
 		else{
-			message.channel.send("<@" + message.author.id + "> You said a total of " + niggerCount + " bad words.");
+			message.channel.send("<@" + message.author.id + "> You said a total of " + count + " bad words.");
 		}
 	}
 	else{
