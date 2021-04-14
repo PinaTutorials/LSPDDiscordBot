d8ddf1130c2542ac178b37fe455b61b5d0aeec22
PinaTutorials <41023844+PinaTutorials@users.noreply.github.com>
2021-04-14 17:47:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ bot.on('message', async message => {
 	else{
 		for(var i = 0; i < fileWords.length; i++){
 			if(messageString === fileWords[i].replace(/(\r\n|\n|\r)/gm, "")){
-				if(fileWords[i] != '' || fileWords[i] != ' '){
+				if(fileWords[i].length > 1){
 					console.log("yes");
 					finalCount++;
 				}
